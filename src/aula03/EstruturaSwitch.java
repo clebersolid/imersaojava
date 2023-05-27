d85989228c99f086f518d33fb216706bd82c68e9
clebersolid <djclebersolid@gmail.com>
2023-05-27 15:21:39 -0300

    imersaojava

--- src/aula03/EstruturaSwitch.java
+++ src/aula03/EstruturaSwitch.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class EstruturaSwitch {
 
-	public static void main(String[] args) {
-		/*Crie um programa que permita ao usu�rio escolher o elogio que gostaria de receber, de acordo com a seguinte regra: 1 - Elogio profissional, 2 - Elogio f�sico, 3 - Elogio pessoal
-		 * Caso digite qualquer outro n�mero, deve ser exibida a mensagem "op��o inv�lida"*/
-		Scanner leitor = new Scanner(System.in);
-		int opcao;
-		System.out.println("ELOGIADOR!");
-		System.out.println("Selecione a op��o desejada:\n1-Elogio profissional\n2-Elogio f�sico\n3-Elogio pessoal");
-		opcao = leitor.nextInt();
-	
-		//Aqui precisamos de uma estrutura para validar a op��o inserida.
+    public static void main(String[] args) {
+        /*Crie um programa que permita ao usu�rio escolher o elogio que gostaria de receber, de acordo com a seguinte regra: 1 - Elogio profissional, 2 - Elogio f�sico, 3 - Elogio pessoal
+         * Caso digite qualquer outro n�mero, deve ser exibida a mensagem "op��o inv�lida"*/
+        Scanner leitor = new Scanner(System.in);
+        int opcao;
+        System.out.println("ELOGIADOR!");
+        System.out.println("Selecione a op��o desejada:\n1-Elogio profissional\n2-Elogio f�sico\n3-Elogio pessoal");
+        opcao = leitor.nextInt();
 
-		switch (opcao) {
-			case 1:
-				System.out.println("Você é um excelenete programador!");
-				break;
+        //Aqui precisamos de uma estrutura para validar a op��o inserida.
 
-			case 2:
-				System.out.println("Você está no shape pro verão!");
-				break;
+        switch (opcao) {
+            case 1:
+                System.out.println("Você é um excelenete programador!");
+                break;
 
-			case 3:
-				System.out.println("Você é o real significado da palavra amizade!");
-				break;
+            case 2:
+                System.out.println("Você está no shape pro verão!");
+                break;
 
-			default:
-				System.out.println("Opção inválida!");
+            case 3:
+                System.out.println("Você é o real significado da palavra amizade!");
+                break;
 
-		}
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
 
-		leitor.close();
+        leitor.close();
 
-	}
+    }
 
 }
