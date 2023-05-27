fd53694c1e4e4e7b572d189d2d4a28289afc6d87
clebersolid <djclebersolid@gmail.com>
2023-05-27 12:08:23 -0300

    modulo 01

---
+++ src/aula02/DesvioComposto.java
@@ -0,0 +1,22 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class DesvioComposto {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double doacao, investimento, usoImediato;
+        System.out.println("Por favor, digite o valor da doação: ");
+        doacao = leitor.nextDouble();
+
+        if (doacao <= 1000.00) {
+            investimento = doacao * 0.05;
+            usoImediato = doacao - investimento;
+        } else {
+            investimento = doacao * 0.15;
+            usoImediato = doacao - investimento;
+        }
+        System.out.println("A doação de R$" + doacao + " implica em um investimento de R$" + investimento + " , restando R$" + usoImediato + " para uso imediato");
+    }
+}
