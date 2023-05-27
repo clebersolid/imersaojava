c1c4f501c6493343220c6a71eebd38c47c4fee0c
clebersolid <djclebersolid@gmail.com>
2023-05-27 12:27:51 -0300

    imersaojava

--- src/aula02/DesvioComposto.java
+++ src/aula02/DesvioComposto.java
@@ -6,16 +6,24 @@ public class DesvioComposto {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        double doacao, investimento, usoImediato;
+        double doacao, investimento = 0, usoImediato = 0;
         System.out.println("Por favor, digite o valor da doação: ");
         doacao = leitor.nextDouble();
 
-        if (doacao > 1 && doacao < 1000.00) {
+        if (doacao < 1) {
+            System.out.println("Valor inválido!");
+
+        } else if (doacao >= 1 && doacao < 1000.00) {
             investimento = doacao * 0.05;
+            usoImediato = doacao - investimento;
+            System.out.println("A doação de R$" + doacao + " implica em um investimento de R$" + investimento + " , restando R$" + usoImediato + " para uso imediato");
         } else {
             investimento = doacao * 0.15;
+            usoImediato = doacao - investimento;
+            System.out.println("A doação de R$" + doacao + " implica em um investimento de R$" + investimento + " , restando R$" + usoImediato + " para uso imediato");
         }
-        usoImediato = doacao - investimento;
-        System.out.println("A doação de R$" + doacao + " implica em um investimento de R$" + investimento + " , restando R$" + usoImediato + " para uso imediato");
+
+
+
     }
 }
